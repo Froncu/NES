84665bc812d527918218e0326c800c63e4715d34
Froncu <jakubfratczak204@gmail.com>
2025-09-27 20:06:26 +0200

    [FEATURE] add the ability to set/clear individual processor status flags

--- source/hardware/CPU/CPU.cpp
+++ source/hardware/CPU/CPU.cpp
@@ -22,10 +22,7 @@ namespace nes
       }
       else
       {
-         auto const opcode = static_cast<Opcode>(read(program_counter_));
-         increment_program_counter();
-
-         switch (opcode)
+         switch (auto const opcode = static_cast<Opcode>(memory_[program_counter_++]))
          {
             case Opcode::BRK_implied:
                current_instruction_ = BRK_implied();
@@ -47,45 +44,38 @@ namespace nes
       return cycle_;
    }
 
-   std::uint8_t CPU::read(std::uint16_t const address) const
+   void CPU::change_processor_status_flag(ProcessorStatusFlag const flag, bool const set)
    {
-      return memory_[address];
-   }
-
-   void CPU::write(std::uint16_t const address, std::uint8_t const data) const
-   {
-      memory_[address] = data;
-   }
-
-   void CPU::increment_program_counter()
-   {
-      ++program_counter_;
+      if (auto const underlying_flag = static_cast<std::underlying_type_t<ProcessorStatusFlag>>(flag); set)
+         processor_status_ |= underlying_flag;
+      else
+         processor_status_ &= ~underlying_flag;
    }
 
    Instruction CPU::BRK_implied()
    {
-      // 2: read next instruction byte (and throw it away), increment PC
+      // read next instruction byte (and throw it away), increment PC
       std::ignore = memory_[program_counter_++];
       co_await std::suspend_always{};
 
-      // 3: push PCH on stack (with B flag set), decrement S
+      // push PCH on stack (with B flag set), decrement S
       processor_status_ |= 0b00010000;
       memory_[0x0100 + stack_pointer_--] = program_counter_ >> 8 & 0x00FF;
       co_await std::suspend_always{};
 
-      // 4: push PCL on stack, decrement S
+      // push PCL on stack, decrement S
       memory_[0x0100 + stack_pointer_--] = program_counter_ & 0x00FF;
       co_await std::suspend_always{};
 
-      // 5: push P on stack, decrement S
+      // push P on stack, decrement S
       memory_[0x0100 + stack_pointer_--] = processor_status_;
       co_await std::suspend_always{};
 
-      // 6: fetch PCL
+      // fetch PCL
       program_counter_ = memory_[0xFFFE];
       co_await std::suspend_always{};
 
-      // 7: fetch PCH
+      // fetch PCH
       program_counter_ |= memory_[0xFFFF] << 8;
       co_return false;
    }
